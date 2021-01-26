78c5c4662a65b3022fcba30e5572b0068e1c2991
Renszo Camacho <varsovia84@gmail.com>
2021-01-26 22:21:34 +0100

    Added a setter and fetch api

--- src/js/Weather.js
+++ src/js/Weather.js
@@ -1,6 +1,13 @@
 export class Weather {
   constructor(city, countryCode) {
+    this.apiKey = "3b03bf2b8e0b398ef104f4cef539b538";
     this.city = city;
     this.countryCode = countryCode;
+  }
+
+  async getWheather() {
+    const URI = `https//api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}`;
+    const data = await fetch(URI);
+    return data;
   }
 }
